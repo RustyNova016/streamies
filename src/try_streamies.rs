330603aae20e7b2a354cdd859ecd73729e78e3c0
RustyNova <50844553+RustyNova016@users.noreply.github.com>
2025-02-28 13:58:37 +0000

    feat: add try_collect_vec

---
+++ src/try_streamies.rs
@@ -0,0 +1,34 @@
+use futures::TryStream;
+use futures::TryStreamExt;
+
+use crate::try_streamies_structs::collect_result_vec::TryCollectVec;
+
+pub trait TryStreamies: TryStream {
+    /// Collect the stream into a vec.
+    ///
+    /// ```
+    /// # futures::executor::block_on(async {
+    /// use futures::stream::{self, StreamExt};
+    /// use streamies::TryStreamies as _;
+    ///
+    /// let stream = stream::iter(vec![Ok::<i32, String>(1), Ok(2), Ok(3)]);
+    ///
+    /// let result = stream.try_collect_vec().await; // No need for ...: Result<Vec<_>, _>!
+    /// assert_eq!(result, Ok(vec![1, 2, 3]));
+    ///
+    /// // However, this will resul in an error value
+    /// let stream = stream::iter(vec![Ok(1), Err("uh oh"), Ok(3)]);
+    ///
+    /// let result = stream.try_collect_vec().await;
+    /// assert_eq!(result, Err("uh oh"));
+    /// # });
+    /// ```
+    fn try_collect_vec(self) -> TryCollectVec<Self>
+    where
+        Self: Sized + TryStreamExt,
+    {
+        TryCollectVec::new(self.try_collect())
+    }
+}
+
+impl<St: TryStream> TryStreamies for St {}
